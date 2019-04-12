4088481e563035e0ef41641c275ab5842a39b259
Dungdt <trungdunghbvn@gmail.coom>
2019-04-13 04:02:14 +0700

    edit docs

---
+++ docs/components/home/Home.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+
+class Home extends React.Component {
+  state = {
+    primaryColor: '#194D33',
+  }
+  render() {
+    return (
+      <div >
+          home
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/alpha/Alpha.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const AlphaPicker = () => {
+
+
+  return (
+    <div>
+        test
+    </div>
+  )
+}
+
+export default ColorWrap(AlphaPicker)
